3392092868e0922a22fa47083c9d0e65fda4acf3
chavi751@gmail.com <chavi751@gmail.com>
2020-02-18 11:50:43 +0200

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'billboards';
+  title = 'Welcome to the billboard management system';
 }
